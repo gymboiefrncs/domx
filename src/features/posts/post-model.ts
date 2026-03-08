6d829fdc717c615561f7aecf13316678412fe4a8
francis <dominiquefrancis42@gmail.com>
2026-03-08 13:17:48 +0800

    fix: remove unnecessary parameters

--- src/features/posts/post-model.ts
+++ src/features/posts/post-model.ts
@@ -1,13 +1,13 @@
-import type { Pool, PoolClient } from "pg";
 import { pool } from "../../config/db.js";
 
 export const insertPost = async (
   body: string,
   userId: string,
   groupId: string,
-  con: Pool | PoolClient = pool,
 ): Promise<void> => {
   const query = `INSERT INTO posts (body, user_id, group_id) VALUES ($1, $2, $3)`;
   const values = [body, userId, groupId];
-  await con.query(query, values);
+  await pool.query(query, values);
 };
+
+export const updatePost = async () => {};
